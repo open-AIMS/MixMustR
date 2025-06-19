47cff45f099f81ffdcc20a32ca7887ed438c00fd
Diego Barneche <barnechedr@gmail.com>
2025-06-19 09:13:36 +0800

    improved style in vignette figures

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -18,7 +18,23 @@ orig_files <- dir(path = "vignettes/", pattern = "*\\.Rmd\\.orig",
                   full.names = TRUE)
 # need to set system variable locally first -------------------------------
 Sys.setenv("NOT_CRAN" = "true")
-purrr::walk(orig_files, ~knitr::knit(.x, file_path_sans_ext(.x)))
+purrr::walk(orig_files, ~knitr::knit(.x, tools::file_path_sans_ext(.x)))
+# fix image html tags -------------------
+new_files <- tools::file_path_sans_ext(orig_files)
+for (i in seq_along(new_files)) {
+  tmp <- readLines(new_files[i])
+  to_fix <- which(grepl("<embed src=", tmp, fixed = TRUE))
+  for (j in seq_along(to_fix)) {
+    target <- (strsplit(tmp[to_fix[j]], "\"")[[1]]) |>
+      grep("\\.pdf$", x = _, value = TRUE)
+    new_tags <- paste0(
+      "<p align=\"center\">\n", "  <img src=\"", target, "\" width = 800/>\n",
+      "</p>\n"
+    )
+    tmp[to_fix[j]] <- new_tags
+  }
+  writeLines(tmp, new_files[i])
+}
 # Move .pdf files into correct directory so they render -------------------
 images <- dir(".", pattern = "vignette-fig.*\\.pdf$")
 success <- file.copy(from = images, to = file.path("vignettes", images),
