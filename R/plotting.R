de03005fe89b8406d79225464cc189cb1276066f
Diego Barneche <barnechedr@gmail.com>
2025-06-23 12:20:43 +0800

    added function to visualise uncertainty in mixing proportions

--- R/plotting.R
+++ R/plotting.R
@@ -31,6 +31,7 @@
 #' @importFrom rlang .data
 #'
 #' @examples
+#' library(MixMustR)
 #' data <- data.frame(
 #'   Observed = c(0.2, 0.4, 0.6, 0.8),
 #'   Predicted = c(0.25, 0.35, 0.65, 0.75),
@@ -67,4 +68,76 @@ plot_multiple_faceted_scatter_avg <- function (data, ...) {
     ylim(c(0, 1)) +
     facet_wrap(~ .data$source) +
     theme_bw()
+}
+
+#' Visually inspect uncertainty for input mixing proportion
+#'
+#' This function plots the expected uncertainty around each input mixing
+#' proportion given its uncertainty on the log scale.
+#'
+#' @param pi A numeric vector containing mixing proportions
+#' (needs to sum to 1). 
+#' @param ln_sigma_rho A numeric vector containing the uncertainty 
+#' around `pi` on the log scale.
+#' @param iter Integer. The number of iterations to run the simulation for.
+#' Defaults to 10,000.
+#' @param seef Integer. Seed for simulation reproducibility. Defaults to 10.
+#' Defaults to 10.
+#'
+#' @return A \code{\link[ggplot2]{ggplot}} object representing the faceted
+# scatter plot.
+#'
+#' @details
+#' We recognise that choosing the appropriate value for the uncertainty around
+#' data stream 2, \eqn{\sigma_{\text{ln}\rho}}, is not trivial. Therefore, the
+#' MixMustR package offers a simple tool which plots the expected variability
+#' in mixing proportion as a function of an input vector of mixing proportions
+#' from data stream 2 (one from each source), and their respective degrees of
+#' confidence or measurement error. Although the function does not inform the
+#' user what the expected uncertainty for each source and observation should be
+#' (what has to come from expect knowledge), it does allow the user to fine
+#' tune \eqn{\sigma_{\text{ln}\rho}} until the appropriate variability in input
+#' mixing proportions is achieved.
+#'
+#' @importFrom ggplot2 ggplot geom_density aes labs scale_x_continuous
+#' @importFrom ggplot2 facet_wrap theme_bw
+#' @importFrom tidyselect everything
+#' @importFrom tidyr pivot_longer
+#' @importFrom stats rnorm
+#' @importFrom rlang .data
+#'
+#' @examples
+#' x = c(0.06, 0.6, 0.04, 0.3)
+#' ln_sigma_x <- rep(1, length(x))
+#' MixMustR::evaluate_uncertainty(x, ln_sigma_x)$plot
+#' 
+#' @export
+evaluate_uncertainty <- function(pi, ln_sigma_rho, iter = 1e4, seed = 10) {
+  softmax <- function(x) {
+    exp(x) / sum(exp(x))
+  }
+  if (length(pi) != length(ln_sigma_rho)) {
+    stop("Arguments `pi` and `ln_sigma_rho` must have the same length.")
+  }
+  if (!is.numeric(pi) | !is.numeric(ln_sigma_rho)) {
+    stop("Arguments `pi` and `ln_sigma_rho` must be numeric vectors.")
+  }
+  ln_x <- log(pi)
+  out <- matrix(0, iter, length(ln_x))
+  set.seed(seed)
+  for (i in seq_len(iter)) {
+    out[i, ] <- rnorm(length(ln_x), ln_x, ln_sigma_rho) |>
+      softmax()
+  }
+  colnames(out) <- paste0("Source ", seq_len(ncol(out)))
+  out <- data.frame(out, check.names = FALSE) |>
+    pivot_longer(everything(), names_to = "Sources", values_to = "Values")
+  plot_out <- ggplot(data = out) +
+    geom_density(mapping = aes(x = .data$Values), trim = TRUE, adjust = 2,
+                 fill = "grey30", alpha = 0.8) +
+    labs(x = "Mixing proportions", y = "Density") +
+    scale_x_continuous(limits = c(0, 1)) +
+    facet_wrap(~.data$Sources, scales = "free_y") +
+    theme_bw()
+  list(data = out, plot = plot_out)
 }
